52e1321d4ad776a23955a8f7c02e904699c5d75a
Teleplandk <57217075+Teleplandk@users.noreply.github.com>
2020-03-30 12:21:57 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,5 +3,5 @@ const app = express();
 const { bindCurrentNamespace } = require("./lib/storage");
 
 // for multi tenancy, place as early as possible to let all
-// modules in your application have access to the tenantId
+// modules in your application have access to the tenantId right away
 app.use(bindCurrentNamespace);
