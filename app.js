984b07da2f3402f73ae9ad8712cc4ce3b9e35f4c
Teleplandk <57217075+Teleplandk@users.noreply.github.com>
2020-03-30 11:55:56 +0200

    Create app.js
    app.js example file

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const app = express();
+const { bindCurrentNamespace } = require("./lib/storage");
+
+// for multi tenancy, place as early as possible to let all
+// modules in your application have access to the tenantId
+app.use(bindCurrentNamespace);
